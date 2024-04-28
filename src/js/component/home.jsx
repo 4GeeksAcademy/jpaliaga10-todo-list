c15100935203a2df91df1932229b1dc9a13dd26e
jpaliaga10 <155824170+jpaliaga10@users.noreply.github.com>
2024-04-28 11:00:26 +0000

    todo-list finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+  const handlers = {
+    handleInputChange: (e) => setInputValue(e.target.value),
+    handleInputKeyDown: (e) => {
+      if (e.key === "Enter") {
+        if (inputValue.trim() !== "") {
+          setTodos([...todos, inputValue.trim()]);
+          setInputValue("");
+        } else {
+          alert("El valor de la tarea no puede ser vacío");
+        }
+      }
+    },
+    handleRemoveTodo: (index) => () =>
+      setTodos(todos.filter((_, currentIndex) => index !== currentIndex))
+  };
+
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="container">
+      <h1>Todos</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={handlers.handleInputChange}
+            value={inputValue}
+            onKeyDown={handlers.handleInputKeyDown}
+            placeholder="What do you need to do"
+          />
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}{" "}
+            <i className="far fa-trash-alt" onClick={handlers.handleRemoveTodo(index)}></i>
+          </li>
+        ))}
+      </ul>
+      <div>Tienes {todos.length} tasks</div>
+    </div>
+  );
 };
 
 export default Home;
+
